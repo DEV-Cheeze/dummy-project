70b1b56e5753856882e67af1e9b255ee10befd16
DEV-Cheeze <ckdwnd9999@gmail.com>
2025-12-05 02:30:38 +0900

    updated deploy.yml

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -3,5 +3,5 @@
 echo "----------서버 시작----------"
 cd /home/ubuntu/dummy-server
 sudo fuser -k -n tcp 8080 || true
-nohup java -jar project.jar
+nohup java -jar project.jar &
 echo "----------서버 시작 완료----------"
