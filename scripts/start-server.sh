ed879ed1fc41f30efbecb94957f8b6ce7206c048
DEV-Cheeze <ckdwnd9999@gmail.com>
2025-12-07 22:00:42 +0900

    docekr CI/CD implementation

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -2,6 +2,6 @@
 
 echo "----------서버 시작----------"
 cd /home/ubuntu/dummy-server
-sudo fuser -k -n tcp 8080 || true
-nohup java -jar project.jar &
+sudo docker compose down || true
+sudo docker compose up -d --build
 echo "----------서버 시작 완료----------"
