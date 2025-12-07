54769164157ee41c392c1ab65d8fd702f27fc401
DEV-Cheeze <ckdwnd9999@gmail.com>
2025-12-07 22:39:38 +0900

    docekr CI/CD implementation

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -2,6 +2,12 @@
 
 echo "----------서버 시작----------"
 cd /home/ubuntu/dummy-server
+
+set -a
+source /home/ubuntu/dummy-server/.env
+set +a
+
+sudo docker pull $AWS_ECR_IMAGE_URL
 sudo docker compose down || true
 sudo docker compose up -d --build
 echo "----------서버 시작 완료----------"
